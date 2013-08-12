f6f0c07e44d7aafc4b5d0637909592c4a43ecfe6
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2013-08-12 12:36:25 +0530

    Rebuilt flotr2 after previous modification

--- flotr2.js
+++ flotr2.js
@@ -7510,9 +7510,11 @@ Flotr.addPlugin('separators', {
 		xcolor: null,
 		xfill: null,
 		xfillopacity: 0.05,
+		xorientation: 0,
 		ycolor: null,
 		yfill: null,
-		yfillopacity: 0.05 
+		yfillopacity: 0.05,
+		yorientation: 0
 	},
 	callbacks: {
 		'flotr:afterdraw': function() { this.separators.insertSeparator(); }
@@ -7541,11 +7543,9 @@ Flotr.addPlugin('separators', {
 		}
 		else
 			return;
-
 		ctx.save();
 		ctx.lineJoin = 'round';
 		ctx.lineWidth = opt.lineWidth;
-
 		if (o.xval)
 		{
 			v = xS(o.xval);
@@ -7560,10 +7560,14 @@ Flotr.addPlugin('separators', {
 				ctx.closePath();
 				if (o.xfill && o.xfillopacity) {
 					ctx.fillStyle = this.processColor(o.xfill, {opacity: o.xfillopacity});
-					ctx.moveTo(of.left, y1);
+					if (o.xorientation === 0)
+						x2 = of.left;
+					else
+						x2 = w + of.left;
+					ctx.moveTo(x2, y1);
 					ctx.lineTo(x1, y1);
 					ctx.lineTo(x1, y2);
-					ctx.lineTo(of.left, y2);
+					ctx.lineTo(x2, y2);
 					ctx.fill();
 				}
 			}
@@ -7573,7 +7577,7 @@ Flotr.addPlugin('separators', {
 			v = yS(o.yval);
 			if (v >= 0 && v <= h)
 			{
-				x1 = of.left; x2 = w + of.left; y1 = v + of.top; y2 = of.top + h;
+				x1 = of.left; x2 = w + of.left; y1 = v + of.top;
 				ctx.strokeStyle = o.ycolor || opt.color;
 				ctx.beginPath();
 				ctx.moveTo(x1, y1);
@@ -7582,6 +7586,10 @@ Flotr.addPlugin('separators', {
 				ctx.closePath();
 				if (o.yfill && o.yfillopacity) {
 					ctx.fillStyle = this.processColor(o.yfill, {opacity: o.yfillopacity});
+					if (o.yorientation === 0)
+						y2 = of.top + h;
+					else
+						y2 = of.top;
 					ctx.moveTo(x1, y2);
 					ctx.lineTo(x1, y1);
 					ctx.lineTo(x2, y1);
